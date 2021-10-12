ab8bab8dbd52b20704a1b27c553948371ad795d7
Gabriel LePoudre <gjl774@mail.usask.ca>
2021-10-12 14:08:11 -0600

    Added Sign-in-sheet
    Added a sign in sheet to make sure everyone will be able to edit scripts

---
+++ TestProject/Assets/TestScripts/Sign_in_sheet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+!! Hoping to use this as a test for everyone to make sure they can edit scripts !!
+!! Just add your name below so we can see that you were able to modify and push a file change !!
+
+>> Gabriel
+>>
+>>
+>>
+>> 
+
+*/
+
+public class Sign_in_sheet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
