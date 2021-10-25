827b53bf52b62a0c0ba5bfd181e031e94b9cdd27
Urnukh Tsogt <t.urnukh@gmail.com>
2021-10-24 21:55:14 -0600

    1st commit
    Luc 1st commit

--- TestProject/Assets/TestScripts/Sign_in_sheet.cs
+++ TestProject/Assets/TestScripts/Sign_in_sheet.cs
@@ -11,6 +11,7 @@ using UnityEngine;
 >> Joel
 >> Ben
 >> Thomas
+>> Lucas
 
 */
 
@@ -19,12 +20,12 @@ public class Sign_in_sheet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
