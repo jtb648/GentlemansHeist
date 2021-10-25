6d9e2c7332c05ae5fa13f08c84d5be5fa2d6d314
g-stru <92223945+g-stru@users.noreply.github.com>
2021-10-25 17:31:21 -0600

    Added Standalone Scripts
    Main Camera and Player movement scripts

---
+++ Gentleman's Heist (W.T.)/Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+
+----Main Camera Script-----
+
+What does this script do?
+- Allows the camera to follow the player
+    --> Glides along with player, with a short delay
+
+- Allows the camera to stay within preset boundaries
+    --> I currently have it set to what my StandAlone project was, we'll need to change these depending on the
+        room sizes we end up choosing for the Store/Banks/Prison/etc.
+
+- Allows us to set a size for the camera
+    --> Prevents confusion in Unity when switching around the editor's layout and keeps boundary
+        values relevant in all situations
+
+*/
+
+
+public class MainCameraScript : MonoBehaviour
+{
+
+    // Reference to Main Camera's camera
+    public Camera cameraMain;
+
+    // The reference to the player
+    public PlayerScript player;
+
+    // The motion delay for the camera, allows for a smoother follow
+    public float delay = 4f;
+
+    // The limits the Camera can go to the right, left, up, and down
+    private float xBoundRight = 20.15f;
+    private float xBoundLeft = -24.7f;
+    private float yBoundTop = 8.95f;
+    private float yBoundBottom = -8.3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Presetting Camera Settings
+        // -- This solidifies the aspect ratio of the Main Camera
+        // -- For now, I've set it to be a 16:10 ratio for our game -> We can change this later if need be :)
+
+        cameraMain.orthographicSize = 5.0f; // Orthographic Size is equal to half the vertical length
+        cameraMain.aspect = (float)  8 / cameraMain.orthographicSize; // The Aspect is a width/height ratio -> 16/10 ratio
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Gathering Player's Location
+        //  --> These Coorindates keep track of the player's current x and y positions
+        float xPos = player.transform.position.x;
+        float yPos = player.transform.position.y;
+
+        // Checking if Player is not within the Camera Boundaries
+        // --> If the player's position exceeds camera right/left boundaries, we don't alter the X Position
+        if(player.transform.position.x <= xBoundLeft || player.transform.position.x >= xBoundRight)
+        {
+            xPos = transform.position.x;
+        }
+        // --> The same follows with the top and bottom boundaries...
+        if(player.transform.position.y >= yBoundTop || player.transform.position.y <= yBoundBottom)
+        {
+            yPos = transform.position.y;
+        }
+
+
+        // Changing the Camera's position
+        //  --> If the current Camera position isn't matching the players...
+        if(transform.position.x != player.transform.position.x || transform.position.y != player.transform.position.y)
+        {
+            // Change Camera movement to the new xPos and yPos
+            // Lerp method allows us to add the delay
+            transform.position = Vector3.Lerp(transform.position, new Vector3(xPos, yPos, transform.position.z), delay);
+        }
+
+    }
+}
+
---
+++ Gentleman's Heist (W.T.)/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+
+----Player Movement Script-----
+
+What does this script do?
+- Allows player to be controlled with WASD or Arrow Keys
+    --> 8 ways of movement (left, right, up, down, and all diagonals)
+
+- Allows boundaries to be set for player
+    --> This needs to be replaced with colliders, it'll make life easier (will do later)
+
+- Allows Walking animations to be determined
+    --> Using a BlendTree in Animator
+
+*/
+
+
+public class PlayerScript : MonoBehaviour
+{
+    
+    // The x and y values for the player to move to
+    private float xMove;
+    private float yMove;
+
+    // The speed the player moves
+    public float speed = 10.0f;
+
+    // Reduce value used for diagonal movement
+    public float reduce = 1.2f;
+
+    // The limits the player can move up, down, left, and right
+    private float yBoundTop = 13.8f;
+    private float yBoundBottom = -13.1f;
+    private float xBoundLeft = -32.5f;
+    private float xBoundRight = 27.8f;
+
+    // Reference to the Player's animator
+    // --> Animator must include xChange, yChange, and walking as its parameters (i can set this up soon)
+    public Animator animator;
+    
+    // Reference to the Player's rigid body
+    public Rigidbody2D myBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+ // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Gather the Keyboard Input from WASD or Arrow Keys
+        // --> The x and y values for the player to move to
+        xMove = Input.GetAxisRaw("Horizontal");;
+        yMove = Input.GetAxisRaw("Vertical");
+
+        // Keeping Player within the Boundaries
+        // --> if player is about to move past the boundaries, set the move value to 0
+        if(transform.position.x + xMove <= xBoundLeft || transform.position.x + xMove >= xBoundRight)
+        {
+            xMove = 0;
+        }
+        if(transform.position.y + yMove >= yBoundTop || transform.position.y + yMove<= yBoundBottom)
+        {
+            yMove = 0;
+        }
+
+        // Diagonal Movement
+        // --> If x and y are both moving, then player will move diagonal
+        if(xMove != 0 && yMove != 0)
+        {
+            // (I wholeheartly believe there is a better way to do this, but it works)
+            // reduced speed on the diagonal, since it moves too fast with regular speed
+            transform.position += new Vector3(xMove * speed/reduce * Time.deltaTime, yMove * speed/reduce * Time.deltaTime, 0);
+            changeAnimation();
+            animator.SetBool("walking", true); // set walking to true
+        }
+        // Right/Left or Up/Down movement
+        // --> If only x or y is moving, then speed can be regular
+        else if(xMove != 0 || yMove != 0)
+        {
+            transform.position += new Vector3(xMove, yMove, 0) * speed * Time.deltaTime;
+            changeAnimation();
+            animator.SetBool("walking", true); // set walking to true
+        }
+        // If no movement, no walking animation is needed
+        else
+        {
+            animator.SetBool("walking", false);
+        }
+    }
+
+    // Updates the animation BlendTree
+    void changeAnimation()
+    {
+            animator.SetFloat("xChange", xMove);
+            animator.SetFloat("yChange", yMove);
+    }
+}
