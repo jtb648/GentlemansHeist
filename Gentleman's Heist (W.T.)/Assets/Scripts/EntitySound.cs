0cf85c69de87c23d6fcdc15391a1f54167d378f4
Gabriel LePoudre <gjl774@mail.usask.ca>
2021-10-26 16:31:55 -0600

    Added basic groundwork for sound
    Created hopefully ideal system for adding a script to a circle that is then added as a child of some entity. Not fully implemented

---
+++ Gentleman's Heist (W.T.)/Assets/Scripts/EntitySound.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Decided to make this a script you attach to some 2d circle, which is attached to an entity that is needed
+ * Will modify the size of the circle
+ */
+public class EntitySound : MonoBehaviour
+{
+    // The ratio at which an objects velocity impacts it's sound. Could be dynamically changed
+    public double speedToRadiusRatio;
+    
+    // The current sound radius that will be displayed
+    private double _currentRadius;
+
+    // The amount of time currentRadius will not be updated with velocity (ie after SetSoundEvent)
+    private int _eventFrames;
+    
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _eventFrames = 0;
+        rb = gameObject.GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_eventFrames > 0)
+        {
+            _eventFrames -= 1;
+        }
+        else
+        {
+            _currentRadius = (rb.velocity.normalized.magnitude) * speedToRadiusRatio;
+        }
+
+        //actually add implementation of radius
+    }
+
+    /*
+     * Given some in game event (ex: tripping) allows outside setting of a custom radius for limited time (frames)
+     * Hint: The game should run at 60fps
+     */
+    public void SetSoundEvent(double newRadius, int numFrames)
+    {
+        _currentRadius = newRadius;
+        _eventFrames = numFrames;
+    }
+
+    public double GetCurrentRadius()
+    {
+        return _currentRadius;
+    }
+}
