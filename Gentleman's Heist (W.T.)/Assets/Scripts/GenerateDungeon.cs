508368f784bb64da0010e207bb373c59606270f7
Thomas <tsf887@usask.ca>
2021-10-26 14:49:12 -0600

    Fixed Scripts

--- Gentleman's Heist (W.T.)/Assets/Scripts/GenerateDungeon.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/GenerateDungeon.cs
@@ -66,7 +66,7 @@ public class GenerateDungeon : MonoBehaviour
         for (int i = 0; i < nRooms; i++)
         {
             int c = 0;
-            //Dont do this at home kids LOL
+        
             while (!GenerateNewRoom())
             {
                 c++;
