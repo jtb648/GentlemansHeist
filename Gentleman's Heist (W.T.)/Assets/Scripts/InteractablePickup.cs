7784794232a16073e966a4aa1a2219bfae6f7b1f
Joel Berryere <76984401+jtb648@users.noreply.github.com>
2021-11-05 10:30:32 -0600

    Added sound for coin pickup and readded coin prefab and score tracker on canvas.
    Added a .wav file for a coin pickup, readded previously added coin png and prefab and readded previously added score text for players total score.

--- Gentleman's Heist (W.T.)/Assets/Scripts/InteractablePickup.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/InteractablePickup.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 //Simple interaction when a Player interacts with an interactable object.
 public class InteractablePickup : MonoBehaviour
 {
+    public AudioSource coinSound;
     //Does the interaction. Placeholder is deleting object and updating UIUpdater texts.
+    void Start() {
+        coinSound = gameObject.GetComponent<AudioSource>();
+    }
     public void DoInteraction(string name){
+        
         if (name.StartsWith("Key")){
             UIUpdater.keys++;
         }
         else if(name.StartsWith("Diamond")){
-            UIUpdater.diamonds++;
+            //AudioSource.PlayClipAtPoint(coinSound.clip,gameObject.transform.position);
+            UIUpdater.diamonds++; 
+        }
+        else if(name.StartsWith("Coin")){
+            AudioSource.PlayClipAtPoint(coinSound.clip,gameObject.transform.position);
+            UIUpdater.score++;
         }
         Destroy(gameObject);
     }
