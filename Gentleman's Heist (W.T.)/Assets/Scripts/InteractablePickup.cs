5a058cc42dbb11fc77a0937932c18a3f22fcce8d
Joel Berryere <76984401+jtb648@users.noreply.github.com>
2021-11-05 18:07:20 -0600

    Added a diamond pickup sound.
    Added a new sound for diamond pickups and changed Interactables to be not as quiet while making a new problem of distorted sound while moving after pickup.

--- Gentleman's Heist (W.T.)/Assets/Scripts/InteractablePickup.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/InteractablePickup.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 //Simple interaction when a Player interacts with an interactable object.
 public class InteractablePickup : MonoBehaviour
 {
+    //Coin pickup sound
     public AudioSource coinSound;
-    //Does the interaction. Placeholder is deleting object and updating UIUpdater texts.
+    //Diamond pickup sound
+    public AudioSource diamondSound;
+    
+    //Instantiating the sound sources
     void Start() {
         coinSound = gameObject.GetComponent<AudioSource>();
+        diamondSound = gameObject.GetComponent<AudioSource>();
     }
+    /**Does an interaction with a game object that depends on which game object is being interacted with. 
+    Key updates number of keys the player has, Diamond updates the number of diamonds a player has and plays audio indicating it was picked up for satisfaction
+    Coin updates the score of the player and plays a coin sound when picked up.
+    After game object is interacted with, it will be deleted from the scene.
+    **/
     public void DoInteraction(string name){
+        //This fixes sound being quiet but distorts the clip if player moves while picking up item
+        Vector3 position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y,Camera.main.transform.position.z);
         
         if (name.StartsWith("Key")){
             UIUpdater.keys++;
         }
         else if(name.StartsWith("Diamond")){
-            //AudioSource.PlayClipAtPoint(coinSound.clip,gameObject.transform.position);
+            AudioSource.PlayClipAtPoint(diamondSound.clip,position, 1.0f);
             UIUpdater.diamonds++; 
         }
         else if(name.StartsWith("Coin")){
-            AudioSource.PlayClipAtPoint(coinSound.clip,gameObject.transform.position);
+            AudioSource.PlayClipAtPoint(coinSound.clip,position, 1.0f);
             UIUpdater.score++;
         }
         Destroy(gameObject);
