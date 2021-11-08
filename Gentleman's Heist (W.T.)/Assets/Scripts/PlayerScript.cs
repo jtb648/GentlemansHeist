50fec77be1ae54af954300e4583bc460fa7f4dac
g-stru <92223945+g-stru@users.noreply.github.com>
2021-11-07 23:41:45 -0600

    Small animation edits

--- Gentleman's Heist (W.T.)/Assets/Scripts/PlayerScript.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/PlayerScript.cs
@@ -125,7 +125,7 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && currentInteractableObject){
             currentInteractableObject.SendMessage("DoInteraction",currentInteractableObject.name);
         }
-
+        
         // Shooting animation logic
         if(Input.GetKeyDown(KeyCode.Space)){
             animator.SetBool("shooting", true); // set shooting to true
