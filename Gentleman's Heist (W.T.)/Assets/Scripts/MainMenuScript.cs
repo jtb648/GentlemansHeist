5e31af9d51815ac02f9c0e0b3adc645f60bf7f66
Joel Berryere <76984401+jtb648@users.noreply.github.com>
2021-11-07 18:26:29 -0600

    Added an actually working and not totally broken looking how to play menu.
    Sorry i'm dumb

--- Gentleman's Heist (W.T.)/Assets/Scripts/MainMenuScript.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/MainMenuScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class MainMenuScript : MonoBehaviour
         Debug.Log("PLAY");
         // Switches to the first Scene here
     }
-
+    public void LoadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
     public void QuitGame(){
         // Stops the Game -> only works once it's built
         Debug.Log("QUIT");
