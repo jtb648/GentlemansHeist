c5ffcf67a6cf9e019715734f23da8633274a1d5a
Gabriel LePoudre <gjl774@mail.usask.ca>
2021-11-08 18:56:09 -0600

    Bullet.cs edit
    Expanded bullet logic for Lucas

--- Gentleman's Heist (W.T.)/Assets/Scripts/Bullet.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/Bullet.cs
@@ -1,11 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
-{   
+{
+    public int rangeFrames = 360;
+
+    void Update()
+    {
+        if (rangeFrames > 0)
+        {
+            rangeFrames -= 1;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("ENTER TAG HERE"))
+        {
+            // Behavior on that specific tag
+            // ex: collision.gameObject.GetComponent<HealthBar>().setHealth(0);
+        }
         Destroy(gameObject, 0.1f);
     }
         
