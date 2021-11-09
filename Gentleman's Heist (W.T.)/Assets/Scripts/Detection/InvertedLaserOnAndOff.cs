05add45a10a11c6ffa7013403316239353151c60
Gabriel LePoudre <gjl774@mail.usask.ca>
2021-11-08 18:24:24 -0600

    Room Prefab hopefully

---
+++ Gentleman's Heist (W.T.)/Assets/Scripts/Detection/InvertedLaserOnAndOff.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertedLaserOnAndOff : MonoBehaviour
+{
+    public int framesToSwap;
+    public Laser laser;
+
+    private int _swapCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _swapCounter = framesToSwap;
+        laser.Off();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_swapCounter > 0)
+        {
+            _swapCounter -= 1;
+        }
+        else
+        {
+            laser.Flip();
+            _swapCounter = framesToSwap;
+        }
+    }
+}
