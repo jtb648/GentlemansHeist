3036b179afe244e9ef45661d1b0d36d993f78ec3
g-stru <92223945+g-stru@users.noreply.github.com>
2021-11-08 18:21:17 -0600

    Pause Menu into Sample Scene
    Press escape to Pause/Unpause game! The buttons refuse to work so I've just hidden them for now :(

--- Gentleman's Heist (W.T.)/Assets/Scripts/PauseMenuScript.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/PauseMenuScript.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuScript : MonoBehaviour
 {
 
+    bool isPaused = false;
+    public GameObject pauseMenu;
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
     public void ResumeGame(){
-        Debug.Log("RESUME");
         // switches back to players last scene
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void PauseGame(){
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPaused = true;
     }
 
     public void BackToMenu(){
-        Debug.Log("BACK TO MENU");
-        // SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
