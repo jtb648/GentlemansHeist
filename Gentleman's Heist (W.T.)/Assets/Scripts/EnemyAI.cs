2b4551b35e394bcbdc85521a39b4e25a45713852
Bennoknight <92766186+Bennoknight@users.noreply.github.com>
2021-11-16 16:21:53 -0600

    Added very broken guard animation

--- Gentleman's Heist (W.T.)/Assets/Scripts/EnemyAI.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/EnemyAI.cs
@@ -38,6 +38,8 @@ public class EnemyAI : MonoBehaviour
     public float bulletForce = 20f;
 
 
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -152,7 +154,6 @@ public class EnemyAI : MonoBehaviour
             Vector2 moveDirection = rb.velocity;
             float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);
-            //RotateToDirection(, 500, 90f);
         }
     }
 
@@ -169,13 +170,6 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private void RotateToDirection(Vector3 target, float RotationSpeed, float offset)
-    {
-        Vector3 dir = target - transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle + offset));
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, RotationSpeed * Time.deltaTime);
-    }
 
     // Update is called once per frame
     void FixedUpdate()
---
+++ Gentleman's Heist (W.T.)/Assets/Scripts/GuardGraphics.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardGraphics : MonoBehaviour
+{
+
+    
+    public Animator animator;
+    private float xMove;
+    private float yMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void rotationStablizer()
+    {
+        float zed = transform.parent.gameObject.transform.rotation.z;
+        transform.rotation = Quaternion.Euler(0, 0, zed);
+    }
+
+    void animationChangerDirectionColorTest()
+    {
+        if (transform.localRotation.eulerAngles.z >= 0 && transform.localRotation.eulerAngles.z <= 90)//down left area
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        }
+        if (transform.localRotation.eulerAngles.z > 90 && transform.localRotation.eulerAngles.z <= 180)//up left area
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        }
+        if (transform.localRotation.eulerAngles.z > 180 && transform.localRotation.eulerAngles.z <= 270)//up right area
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        }
+        if (transform.localRotation.eulerAngles.z > 270 && transform.localRotation.eulerAngles.z <= 359)//down right area
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+        }
+    }
+
+
+
+    //The animation stuff is a mess and needs fixing
+
+
+
+
+    void changeAnimation()
+    {
+        animator.SetFloat("xChange", xMove);
+        animator.SetFloat("yChange", yMove);
+    }
+
+    private void FixedUpdate()
+    {
+        xMove = Input.GetAxisRaw("Horizontal");
+        yMove = Input.GetAxisRaw("Vertical");
+        if (xMove != 0 || yMove != 0)
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);//debug
+            changeAnimation();
+            animator.SetBool("walking", true); // set walking to true
+            //walkingSound.UnPause();
+
+        }
+        // If no movement, no walking animation is needed
+        else
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);//debug
+            animator.SetBool("walking", false);
+            //walkingSound.Pause();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rotationStablizer();
+        //animationChangerDirectionColorTest();
+    }
+}
