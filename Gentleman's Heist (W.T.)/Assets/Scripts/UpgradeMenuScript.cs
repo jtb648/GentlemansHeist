7df0c09c8e82133000393069546bf8367032e6d8
g-stru <92223945+g-stru@users.noreply.github.com>
2021-11-16 12:25:01 -0600

    Started creating UpgradeMenu script
    So far Current Score and Money are connected

--- Gentleman's Heist (W.T.)/Assets/Scripts/UpgradeMenuScript.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/UpgradeMenuScript.cs
@@ -2,10 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class UpgradeMenuScript : MonoBehaviour
 {
+
+    // ----MissionComplete-----
+    // https://learn.unity.com/tutorial/working-with-textmesh-pro#5f86410eedbc2a00249a4928
+    [SerializeField]    
+    private TMP_Text currentScore;
+
+
+    //----Upgrade Menu-----
+    [SerializeField]    
+    private TMP_Text earnings;
+    
+    void Start(){
+        currentScore.text = "Current Score: " + PlayerData.GetScore();
+        earnings.text = "Earnings: " + PlayerData.GetMoney();
+    }
+
     public void ContinueNextLevel(){
         SceneManager.LoadScene("SampleScene");
     }
+
+    public void buySilentShoes(){
+
+    }
+
+    public void buyLockpick(){
+
+    }
+
+    public void buySilentWeapon(){
+
+    }
+
 }
