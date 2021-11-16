0cbe7a4b2a14832d7c7bcc6483f499358a4bc775
g-stru <92223945+g-stru@users.noreply.github.com>
2021-11-16 12:57:43 -0600

    Upgrade buttons reduce PlayerData's money

--- Gentleman's Heist (W.T.)/Assets/Scripts/UpgradeMenuScript.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/UpgradeMenuScript.cs
@@ -18,10 +18,42 @@ public class UpgradeMenuScript : MonoBehaviour
     //----Upgrade Menu-----
     [SerializeField]    
     private TMP_Text earnings;
+    [SerializeField] 
+    private TMP_Text silentShoesPriceText;
+    [SerializeField] 
+    private TMP_Text lockPickPriceText;
+    [SerializeField] 
+    private TMP_Text silentWeaponPriceText;
+
+    [SerializeField] 
+    private TMP_Text coffeePriceText;
+    [SerializeField] 
+    private TMP_Text alarmDisablerPriceText;
+    [SerializeField] 
+    private TMP_Text foodPriceText;
+    private int silentShoesPrice;
+    private int lockPickPrice;
+    private int silentWeaponPrice;
+    private int coffeePrice;
+    private int alarmDisablerPrice;
+    private int foodPrice;
     
     void Start(){
         currentScore.text = "Current Score: " + PlayerData.GetScore();
         earnings.text = "Earnings: " + PlayerData.GetMoney();
+        // Setting Prices and Button Text
+        lockPickPrice = 300;
+        lockPickPriceText.text = "" + lockPickPrice;
+        silentShoesPrice = 400;
+        silentShoesPriceText.text = "" + silentShoesPrice;
+        silentWeaponPrice = 500;
+        silentWeaponPriceText.text = "" + silentWeaponPrice;
+        coffeePrice = 200;
+        coffeePriceText.text = "" + coffeePrice;
+        alarmDisablerPrice = 600;
+        alarmDisablerPriceText.text = "" + alarmDisablerPrice;
+        foodPrice = 100;
+        foodPriceText.text = "" + foodPrice;
     }
 
     public void ContinueNextLevel(){
@@ -29,15 +61,45 @@ public class UpgradeMenuScript : MonoBehaviour
     }
 
     public void buySilentShoes(){
-
+        // [Add SilentShoes to inventory here(?)]
+        int money = PlayerData.GetMoney() - silentShoesPrice;
+        PlayerData.SetMoney(money);
+        earnings.text = "Earnings: " + money;
     }
 
     public void buyLockpick(){
-
+        // [Add Lockpick to inventory here(?)]
+        int money = PlayerData.GetMoney() - lockPickPrice;
+        PlayerData.SetMoney(money);
+        earnings.text = "Earnings: " + money;
     }
 
     public void buySilentWeapon(){
+        // [Add SilentWeapon to inventory here(?)]
+        int money = PlayerData.GetMoney() - silentWeaponPrice;
+        PlayerData.SetMoney(money);
+        earnings.text = "Earnings: " + money;
+    }
 
+    public void buyCoffee(){
+        // [Add Coffee to inventory here(?)]
+        int money = PlayerData.GetMoney() - coffeePrice;
+        PlayerData.SetMoney(money);
+        earnings.text = "Earnings: " + money;
+    }
+
+    public void buyAlarmDisabler(){
+        // [Add AlarmDisabler to inventory here(?)]
+        int money = PlayerData.GetMoney() - alarmDisablerPrice;
+        PlayerData.SetMoney(money);
+        earnings.text = "Earnings: " + money;
+    }
+
+    public void buyFood(){
+        // [Add Food to inventory here(?)]
+        int money = PlayerData.GetMoney() - foodPrice;
+        PlayerData.SetMoney(money);
+        earnings.text = "Earnings: " + money;
     }
 
 }
