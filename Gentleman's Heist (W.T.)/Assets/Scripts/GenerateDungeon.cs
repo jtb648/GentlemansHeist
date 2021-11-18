2010ffeade03ef32b414f15388a0802dc256a3d7
Thomas <tsf887@usask.ca>
2021-11-18 14:41:56 -0600

    Explosion, stairs Update

--- Gentleman's Heist (W.T.)/Assets/Scripts/GenerateDungeon.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/GenerateDungeon.cs
@@ -73,7 +73,7 @@ public class GenerateDungeon : MonoBehaviour
         directionsList.Add(new Vector2(1, 0));
         directionsList.Add(new Vector2(-1, 0));
 
-        Vector2 pos = new Vector2(8, 7);
+        Vector2 pos = new Vector2(9, 9);
         Vector2 size = new Vector2(Random.Range(1, 2), Random.Range(1, 2));
         Tuple<Vector2, Vector2> firstRoom = new Tuple<Vector2, Vector2>(pos, size);
         roomPositions.Add(firstRoom);
