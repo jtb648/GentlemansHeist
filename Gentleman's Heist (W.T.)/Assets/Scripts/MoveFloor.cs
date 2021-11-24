1366d9d610afdd440fae8bdb648fd68b07ed3855
Thomas <tsf887@usask.ca>
2021-11-24 15:42:25 -0600

    Stairs will now be locked until key is collected

--- Gentleman's Heist (W.T.)/Assets/Scripts/MoveFloor.cs
+++ Gentleman's Heist (W.T.)/Assets/Scripts/MoveFloor.cs
@@ -7,13 +7,24 @@ public class MoveFloor : MonoBehaviour
 {
     Game game;
 
+    public Sprite newSprite;
+
+    private bool canUseStairs = false;
+
     void Start() {
         game = FindObjectOfType<Game>();
     }
 
+    void Update() {
+        if (PlayerData.GetKeys() >= 1) {
+            this.GetComponent<SpriteRenderer>().sprite = newSprite;
+            canUseStairs = true;
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player") {
-            if (PlayerData.GetKeys() >= 1) { //Left at zero for debugging purposes, change zero to one before building
+            if (canUseStairs) {
                 PlayerData.ChangeKeys(PlayerData.GetKeys()-1);
                 PlayerData.NextLevel();
                 game.NextFloor();
